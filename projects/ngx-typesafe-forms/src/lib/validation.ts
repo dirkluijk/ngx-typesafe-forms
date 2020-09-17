6be93188292d24abe30b3af0c1f2a00cbfb6db2b
Dirk Luijk <dluijk@bol.com>
2020-09-17 18:55:03 +0200

    fix: make validator functions more relaxed

--- projects/ngx-typesafe-forms/src/lib/validation.ts
+++ projects/ngx-typesafe-forms/src/lib/validation.ts
@@ -6,8 +6,13 @@ import { Observable } from 'rxjs';
 
 import { AbstractControl } from './abstract-control';
 
-export type ValidatorFn<T> = (control: AbstractControl<T>) => ValidationErrors | null;
-export type AsyncValidatorFn<T> = (control: AbstractControl<T>) => Promise<ValidationErrors | null> | Observable<ValidationErrors | null>;
+export type ValidatorFn<T> =
+  ((control: AbstractControl<T>) => ValidationErrors | null) |
+  ((control: AbstractControl<T | null>) => ValidationErrors | null);
+
+export type AsyncValidatorFn<T> =
+  ((control: AbstractControl<T>) => Promise<ValidationErrors | null> | Observable<ValidationErrors | null>) |
+  ((control: AbstractControl<T | null>) => Promise<ValidationErrors | null> | Observable<ValidationErrors | null>);
 
 export interface AbstractControlOptions<T> extends AngularAbstractControlOptions {
   validators?: ValidatorFn<T> | ValidatorFn<T>[] | null;
